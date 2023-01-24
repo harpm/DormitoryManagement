40f04184aa331fa1ebcfe1e8b692579be6a92b80
Arsalan Hadidi <arsalan.hadidi1379@gmail.com>
2023-01-25 00:21:38 +0330

    Dorm table fixed and poppulated.

---
+++ Populating and Fixing Dorm Table.sql
@@ -0,0 +1,31 @@
+﻿USE DormitoryManagement;
+
+GO
+
+-- Fixing spelling mistake --
+
+ALTER TABLE Dorm
+	DROP COLUMN Possibilies;
+ALTER TABLE Dorm
+	ADD Possibilities NVARCHAR(250);
+
+GO
+
+-- Type: 0 => normal, 1 => locked, 2 => Married, 3 => StaffOnly
+INSERT INTO [dbo].Dorm (Capacity, NumberOfRooms, [Type], [Address], Possibilities)
+VALUES (150, 30, 3, N'بلوار شهید احمدی', N'D')
+
+GO
+
+INSERT INTO [dbo].Dorm (Capacity, NumberOfRooms, [Type], [Address], Possibilities)
+VALUES (150, 30, 2, N'بلوار شهید باستانی', N'D')
+
+GO
+
+INSERT INTO [dbo].Dorm (Capacity, NumberOfRooms, [Type], [Address], Possibilities)
+VALUES (200, 50, 0, N'بلوار شهید کشوری', N'T')
+
+GO
+
+INSERT INTO [dbo].Dorm (Capacity, NumberOfRooms, [Type], [Address], Possibilities)
+VALUES (100, 25, 1, N'بلوار زرینکوب', N'T')
