c1d30632a4bc0ae0100c95a3948a1a36e6b6c27f
Arsalan Hadidi <arsalan.hadidi1379@gmail.com>
2023-01-25 00:20:59 +0330

    Managers table poppulator added.

---
+++ Populating Managers Table.sql
@@ -0,0 +1,21 @@
+﻿USE DormitoryManagement;
+
+GO
+
+INSERT INTO [dbo].Manager(FirstName, LastName, [Address], PhoneNumber, UserName, [Password])
+VALUES (N'محسن', N'جلیلی', N'شهرک کاوه', '09384373010', 'mohsen_j', 'mohsen1355')
+
+INSERT INTO [dbo].Manager(FirstName, LastName, [Address], PhoneNumber, UserName, [Password])
+VALUES (N'فاطمه', N'امیری', N'شهرک میلاد', '09484475012', 'fatami', 'fat0912')
+
+
+INSERT INTO [dbo].Manager(FirstName, LastName, [Address], PhoneNumber, UserName, [Password])
+VALUES (N'محمد', N'اعتمادی', N'بهارستان', '09984385410', 'mhammad_ee12', 'ehim099')
+
+
+INSERT INTO [dbo].Manager(FirstName, LastName, [Address], PhoneNumber, UserName, [Password])
+VALUES (N'حمید', N'فرشادی', N'شهرک کشوری', '09385303120', 'hamidreza', 'hamidfr12')
+
+
+INSERT INTO [dbo].Manager(FirstName, LastName, [Address], PhoneNumber, UserName, [Password])
+VALUES (N'منیره', N'بهنژاد', N'شهرک منتظری', '09784356010', 'monaw', 'mb1334')
