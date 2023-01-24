555b1b327d24fe9adaf456dc7921edc4ad6bdba0
Arsalan Hadidi <arsalan.hadidi1379@gmail.com>
2023-01-25 00:20:16 +0330

    Added Poppulator query for students table.

---
+++ Populating Students Table.sql
@@ -0,0 +1,63 @@
+﻿USE DormitoryManagement;
+
+GO
+
+INSERT INTO [dbo].Student(FirstName, LastName, NationalCode, PhoneNumber, [Address])
+VALUES('Arsalan', 'Tavakoli', '1283371554', 09995443005, N'خیابان ستاری')
+
+GO
+
+INSERT INTO [dbo].Student(FirstName, LastName, NationalCode, PhoneNumber, [Address])
+VALUES('Shahram', 'Mohammadi', '1293381564', 09695743005, N'خیابان پینک فبوید')
+
+GO
+
+INSERT INTO [dbo].Student(FirstName, LastName, NationalCode, PhoneNumber, [Address])
+VALUES('Fatemeh', 'Hassanzadeh', '1253781568', 09695883006, N'خیابان فروغی')
+
+GO
+
+INSERT INTO [dbo].Student(FirstName, LastName, NationalCode, PhoneNumber, [Address])
+VALUES('زهرا', 'حمید نژاد', '1254781578', 09695884506, N'خیابان فروغی')
+
+GO
+
+INSERT INTO [dbo].Student(FirstName, LastName, NationalCode, PhoneNumber, [Address])
+VALUES('Armita', 'Farrokhy', '1245781578', 09695684506, N'خیابان آمادگاه')
+
+GO
+
+INSERT INTO [dbo].Student(FirstName, LastName, NationalCode, PhoneNumber, [Address])
+VALUES('Sara', 'Daroogar', '1293450224', 09495864556, N'خیابان احمد آباد')
+
+GO
+
+INSERT INTO [dbo].Student(FirstName, LastName, NationalCode, PhoneNumber, [Address])
+VALUES('Parmiss', 'Zare', '1293450234', 09499664557, N'خیابان فردوسی')
+
+GO
+
+INSERT INTO [dbo].Student(FirstName, LastName, NationalCode, PhoneNumber, [Address])
+VALUES('Jamshid', 'Mokhtari', '1293450226', 09499664557, N'خیابان فردوسی')
+
+GO
+
+INSERT INTO [dbo].Student(FirstName, LastName, NationalCode, PhoneNumber, [Address])
+VALUES('Ardalan', 'Shoja Kaveh', '1293450266', 09499664557, N'خیابان لاهور')
+
+GO
+
+INSERT INTO [dbo].Student(FirstName, LastName, NationalCode, PhoneNumber, [Address])
+VALUES('Shamim', 'Sameti', '1293453424', 09349664557, N'خیابان لاهور')
+
+GO
+
+INSERT INTO [dbo].Student(FirstName, LastName, NationalCode, PhoneNumber, [Address])
+VALUES('Samin', 'Sameti', '1293120224', 09349654659, N'خیابان لاهور')
+
+GO
+
+INSERT INTO [dbo].Student(FirstName, LastName, NationalCode, PhoneNumber, [Address])
+VALUES('Dariush', 'Ghasemi', '1093450224', 09341644659, N'خیابان هزارجریب')
+
+GO
